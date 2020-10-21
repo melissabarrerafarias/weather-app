d6a5a4aeaaa3a50adc224eb4175cacaa09e8a5bd
melissikiss <melissikiss@gmail.com>
2020-10-20 19:13:28 -0700

    added event listener to form and verified correct cities were being targeted

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,12 @@
 // api key
 // http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=1b885e610b5ff8d663b1c0a52218b536
 
-var getCityLocation = function (city) {
+// these two variables target the form element (searchFormEl) and the input element (searchInputEl) in the HTML
+var searchFormEl = document.querySelector("#search-city");
+var searchInputEl = document.querySelector("#search-input")
+
+
+var getCityWeather = function (city) {
     // api url for weather
     var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=1b885e610b5ff8d663b1c0a52218b536";
 
@@ -11,6 +16,22 @@ var getCityLocation = function (city) {
             console.log(data);
         });
     });
-console.log("outside")
 }
-getCityLocation("San Jose");
+
+var submitCity = function(event) {
+    event.preventDefault();
+    // get the city from search bar 
+    var locationChosen = searchInputEl.value.trim();
+
+    // verify they chose a city and clear input
+    if (locationChosen) {
+        getCityWeather(locationChosen);
+        searchInputEl.value = "";
+    }
+    else {
+        alert("Please enter the city you would like to see!");
+    }
+}
+
+searchFormEl.addEventListener("submit", submitCity);
+
